4204af6b398e9e122023b90602103beaa1842815
Ghost <rildiarov@gmail.com>
2024-07-18 16:18:35 +0400

    worked project

---
+++ src/main/java/model/Description.java
@@ -0,0 +1,17 @@
+package com.example.crptapi.model;
+
+public class Description {
+    private String descriptionText;
+
+    public Description(String descriptionText) {
+        this.descriptionText = descriptionText;
+    }
+
+    public String getDescriptionText() {
+        return descriptionText;
+    }
+
+    public void setDescriptionText(String descriptionText) {
+        this.descriptionText = descriptionText;
+    }
+}
